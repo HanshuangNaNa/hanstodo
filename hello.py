5ec67b92545f214838e0a1b2654db36dc8b1ec1d
HanshuangNaNa <hanshuangnana@gmail.com>
2018-04-18 22:39:55 +0800

    daily

--- hello.py
+++ hello.py
@@ -16,4 +16,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80),unique=True,nullable=False)
+    email = db.Column(sb.String(120),unique=True,nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
 
