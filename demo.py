65f66d76ba6e1f1d4bd1216dc4b1529986a49c8b
HanshuangNaNa <hanshuangnana@gmail.com>
2018-04-19 22:06:43 +0800

    daily

---
+++ demo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+"""
+# Author: HansHuang
+# Created Time : 2018年04月19日 星期四 22时05分08秒
+# File Name: demo.py
+# Description:
+"""
+import logging
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s : %(levelname)s %(message)s',datefmt='%Y-%m-%d %A %H:%M:%S')
