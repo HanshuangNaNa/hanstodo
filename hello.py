cfc8468127d7d08136b2c76ea577ab8617fec20b
HanshuangNaNa <hanshuangnana@gmail.com>
2018-04-20 23:08:26 +0800

    daily

--- hello.py
+++ hello.py
@@ -8,7 +8,8 @@
 """
 import logging
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s : %(levelname)s %(message)s',datefmt='%Y-%m-%d %A %H:%M:%S')
 
@@ -19,10 +20,34 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80),unique=True,nullable=False)
-    email = db.Column(sb.String(120),unique=True,nullable=False)
+    email = db.Column(db.String(120),unique=True,nullable=False)
+
+    def __init__(self,username,email):
+        self.username = username
+        self.email = email
 
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80),nullable=False)
+    body = db.Column(db.Text,nullable=False)
+    pub_date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
 
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'),nullable=False)
+    category = db.relationship('Category',backref=db.backref('posts',lazy=True))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50),nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+
+    
 
