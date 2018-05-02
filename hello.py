c28983c0963f451c743f35e3a7e0b15886ea16e8
HanshuangNaNa <hanshuangnana@gmail.com>
2018-05-02 22:27:00 +0800

    daily

--- hello.py
+++ hello.py
@@ -17,21 +17,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-db = SQLALCHEMY(app)
+db = SQLAlchemy(app)
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
     
-    users = db.relationship('User',backref='role')
+    users = db.relationship('User',backref='role',lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key)
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64),unique=True,index=True)
 
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
